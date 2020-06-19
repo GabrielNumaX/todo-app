02a6d8d516cb9074bde49168da16b50d46b53eac
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2020-06-18 22:12:11 -0300

    Updates

---
+++ precache-manifest.93fa42b33492e47bc9a1c52db74da2c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90744ec3ee16beccbbdf5ac0d94ce27",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "98f6b42247c36b85f263",
+    "url": "/todo-app/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "51cd1e03055efd7c90f4",
+    "url": "/todo-app/static/css/main.4ab2da01.chunk.css"
+  },
+  {
+    "revision": "98f6b42247c36b85f263",
+    "url": "/todo-app/static/js/2.a27b42ce.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/todo-app/static/js/2.a27b42ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51cd1e03055efd7c90f4",
+    "url": "/todo-app/static/js/main.872336a4.chunk.js"
+  },
+  {
+    "revision": "017594b70fcddfe941fd",
+    "url": "/todo-app/static/js/runtime-main.94e975c4.js"
+  },
+  {
+    "revision": "2719760172113d02d4f90892f5ef4ed3",
+    "url": "/todo-app/static/media/cool-background.27197601.png"
+  }
+]);
