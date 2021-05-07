00dd58d53b83a0ce9a8121e2eff83a1ba7123e7a
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2021-05-06 22:21:59 -0300

    path fix

--- server/server.js
+++ server/server.js
@@ -31,11 +31,11 @@ app.use('/password', resetRoute);
 if (process.env.NODE_ENV === 'production') {
 
     //serves react app  
-    app.use(express.static(__dirname, '../build'));
+    app.use(express.static(__dirname, '../', 'build'));
 
     app.get('*', (req, res) => {
 
-        res.sendFile(path.join(__dirname, '../build', 'index.html')) //relative path
+        res.sendFile(path.join(__dirname, '../', 'build', 'index.html')) //relative path
     })
 
 }
